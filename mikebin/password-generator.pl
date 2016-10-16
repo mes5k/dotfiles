2e0d865b45003c6edc94dee52137ef3c5cef0bff
Mike Smoot <mes@aescon.com>
2016-10-16 16:00:11 -0700

    simplified password gen

--- mikebin/password-generator.pl
+++ mikebin/password-generator.pl
@@ -1,7 +1,5 @@
 #!/usr/bin/perl
-my @chars = ('A'..'Z', '0'..'9', 'a'..'z'); 
-my @moreChars = split //, "~!@#$%^&*()*_+`-={}|[]\;':<>?,./";
-for my $c (@moreChars) { push @chars, $c; }
+my @chars = ('A'..'Z', '0'..'9', 'a'..'z', '!', '@', '#', '$', '%', '^', '&', '*', ':', ';');
 my $len = 10 + int(rand(4));
 my $rpass = '';
 for(my $i = 0; $i < $len; ++$i) {
